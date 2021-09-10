4c913362132ddb185c523337e41fa64d240fc580
Alwyn Grant <marzgrant@gmail.com>
2021-09-10 13:35:11 -0700

    added signup form and css file(empty)

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
 
 function LoginFormPage() {
-    const dispatch = useDispatch();
-    const sessionUser = useSelector(state => state.session.user);
     const [credential, setCredential] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState([]);
 
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    
     if (sessionUser) return <Redirect to="/" />;
 
     const handleSubmit = (e) => {
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -7,6 +7,21 @@ const REMOVE_USER = 'session/removeUser';
 const setUser = (user) => { return { type: SET_USER, payload: user } };
 const removeUser = () => { return { type: REMOVE_USER } };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
@@ -28,6 +43,14 @@ export const login = (user) => async (dispatch) => {
 //     dispatch(removeUser());
 //     return response;
 // }
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 
 const sessionReducer = (state = { user: null }, action) => {
     let newState;
