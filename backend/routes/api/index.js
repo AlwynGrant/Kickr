5d3687cdb5529099fb8f01216f62ee0dfa4c070c
Alwyn Grant <marzgrant@gmail.com>
2021-09-09 17:37:43 -0700

    added files AND completed part one!

---
+++ backend/routes/api/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+//test
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+module.exports = router;
