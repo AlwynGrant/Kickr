b34dea13e7b53023a421704452d618ebdcc42c2b
Alwyn Grant <marzgrant@gmail.com>
2021-09-09 18:11:15 -0700

    completed redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import configureStore from './store';
+
+const store = configureStore();
+
+//Set up to only show in development, NEVER expose to production
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+//Root Reducer
+const rootReducer = combineReducers({
+
+});
+
+// Change depending on if in production or development
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+// Will use this function to attach Redux store to React app
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
