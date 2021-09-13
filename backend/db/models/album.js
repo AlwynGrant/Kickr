a5c8a0693afb0c4556c5c362c8f39179be50151e
Alwyn Grant <marzgrant@gmail.com>
2021-09-13 14:20:41 -0700

    re-added remaining tables

--- backend/db/models/album.js
+++ backend/db/models/album.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   Album.associate = function(models) {
     // associations can be defined here
     Album.belongsTo(models.User, { foreignKey: 'userId' });
+    Album.hasMany(models.Image, { foreignKey: 'albumId' });
   };
   return Album;
 };
---
+++ backend/db/models/image.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Image = sequelize.define('Image', {
+    userId: DataTypes.INTEGER,
+    albumId: DataTypes.INTEGER,
+    imageUrl: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  Image.associate = function(models) {
+    // associations can be defined here
+    Image.belongsTo(models.User, { foreignKey: 'userId' });
+    Image.hasMany(models.Comment, { foreignKey: 'imageId' });
+  };
+  return Image;
+};
