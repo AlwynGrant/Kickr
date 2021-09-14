3a625504114cb525a773461a46934ecc77485516
Alwyn Grant <marzgrant@gmail.com>
2021-09-14 10:23:51 -0700

    started backend route -- new image

---
+++ backend/routes/api/new-image.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { Image } = require('../../db/models');
+const router = express.Router();
+
+const validateImage = [
+    check('imageUrl')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a valid url.')
+];
+
+
+router.post("/", validateImage, asyncHandler(async (req, res) => {
+    //check if the user is logged in, if not-- redirect
+    // -- have this checked in frontend!
+
+    //parse the request body
+    const { imageUrl, description } = req.body;
+    //build a new image using the parsed data
+    const image = await Image.build({ imageUrl, description });
+    //erect validations as a constant
+    const validationErrors = validationResult(req);
+    //check if validations are empty
+    if (validationErrors.isEmpty()) {
+        //if so, save the new image
+        await image.save();
+        //redirect
+        return res.redirect('/user');
+    } else {
+        //if not, create an errors array
+        const errors = validationErrors.array().map((error) => error.msg);
+        // send back errors
+        return res.json(errors)
+    }
+}))
+
+
+module.exports = router;
