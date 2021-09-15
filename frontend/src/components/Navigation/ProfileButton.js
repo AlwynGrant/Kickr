2ac01fc68385f28dfa8021312e1327b5e6da19b5
Alwyn Grant <marzgrant@gmail.com>
2021-09-15 09:47:47 -0700

    major bug fixes for c(r)ud action. working on validitions now.

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router";
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
+    const history = useHistory();
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
 
@@ -21,6 +23,7 @@ function ProfileButton({ user }) {
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
+        history.push('/')
     };
 
     return (
--- frontend/src/components/UserPage/index.js
+++ frontend/src/components/UserPage/index.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useParams } from 'react-router';
 import { useEffect } from 'react';
 import { listImages } from '../../store/image';
 import '../../reset.css'
@@ -7,25 +8,33 @@ import './UserPage.css'
 
 
 function UserPage() {
+    const { userId } = useParams();
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-    const images = useSelector(state => Object.values(state.image)[0]);
-    console.log(images);
-
-    // if (!sessionUser) return <Redirect to="/" />
+    const images = useSelector(state => state.image.images);
 
     useEffect(() => {
-        dispatch(listImages(sessionUser.id))
-    }, [dispatch, sessionUser.id]);
+        dispatch(listImages(userId));
+    }, [dispatch, userId]);
+
+    const isLogged = () => {
+        if (!sessionUser) return
+    }
+
 
     return (
             <div className='user-container'>
-                <NavLink to='/image'>Upload new Image</NavLink>
+                {
+                <NavLink onLoad={isLogged} to='/image'>Upload new Image</NavLink>
+                }
             {
                 // TODO: MAP OUT IMAGES ONCE THIS COMPONENT HAS ACCESS TO THEM
-                // images.map((image) => {
-                //     return <div className='image-box'>{image}</div>
-                // })
+                images?.map((image) => {
+                    return <div key={image.id} className='image-box'>
+                        {image.id}
+                        {image.imageUrl}
+                        </div>
+                })
             }
 
             </div>
