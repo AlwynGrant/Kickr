cfa78f939993a7010248f21d8935224c2fffff48
Alwyn Grant <marzgrant@gmail.com>
2021-09-15 21:09:20 -0700

    setup frontend CRUD ops in state

---
+++ frontend/src/store/comment.js
@@ -0,0 +1,119 @@
+import { csrfFetch } from './csrf';
+
+// --------------------------- Defined Action Types as Constants ---------------------
+
+
+
+// const GET_COMMENT = 'users/GET_COMMENT'
+const ADD_COMMENT = 'users/ADD_COMMENT';
+// const EDIT_COMMENT = 'users/EDIT_COMMENT';
+// const REMOVE_COMMENT = 'users/REMOVE_COMMENT';
+
+
+// --------------------------- Defined Action Creator(s) --------------------------
+
+
+// const getComments = (comment) => ({ type: GET_COMMENTS, comment });
+const addComment = (comment) => ({ type: ADD_COMMENT, comment });
+// const editComment = (comment) => ({ type: EDIT_COMMENT, comment });
+// const removeComment = () => ({ type: REMOVE_COMMENT });
+
+
+// ---------------------------  Defined Thunk(s) --------------------------------
+
+
+// create comment
+export const createComment = (newComment) => async (dispatch) => {
+    const { userId, imageId, comment } = newComment;
+    const response = await csrfFetch(`/api/${imageId}/comment`, {
+        method: 'POST',
+        body: JSON.stringify({
+            userId,
+            imageId,
+            comment
+        })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(addComment(data));
+        return response;
+    };
+};
+
+
+// // get image(s)
+// export const listImages = (userId) => async (dispatch) => {
+//     const response = await csrfFetch(`/api/user/${userId}`, {
+//         method: 'GET'
+//     });
+
+//     if (response.ok) {
+//         const images = await response.json();
+//         dispatch(getImages(images));
+//     }
+// }
+
+// // edit image data
+// export const editDescription = (imageId, updatedState) => async (dispatch) => {
+//     const response = await csrfFetch(`/api/image/${imageId}/edit`, {
+//         method: 'PATCH',
+//         body: JSON.stringify({ description: updatedState })
+//     });
+
+//     if (response.ok) {
+//         const data = await response.json();
+//         dispatch(editImage(data));
+//         return response;
+//     };
+// };
+
+
+// // delete an image
+// export const deleteImage = (imageId) => async (dispatch) => {
+//     const response = await csrfFetch(`/api/image/${imageId}/delete`, {
+//         method: 'DELETE',
+//     });
+
+//     if (response.ok) {
+//         const data = await response.json();
+//         dispatch(removeImage(data));
+//     };
+// };
+
+
+// Define an initial state
+const initialState = {};
+
+
+// Define a reducer
+const commentReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case ADD_COMMENT:
+            newState = Object.assign({}, state);
+            newState = action.comment;
+            return newState;
+        // case GET_COMMENTS:
+        //     newState = Object.assign({}, state);
+        //     newState = action.comment;
+        //     return newState;
+        // case GET_COMMENT:
+        //     newState = Object.assign({}, state);
+        //     newState = action.comment;
+        //     return newState;
+        // case EDIT_COMMENT:
+        //     newState = Object.assign({}, state);
+        //     newState.comment.comment = action.comment.comment;
+        //     return newState;
+        // case REMOVE_COMMENT:
+        //     newState = Object.assign({}, state);
+        //     delete newState[action.comment]
+        //     return newState;
+        default:
+            return state;
+    }
+}
+
+// Export the reducer
+export default commentReducer;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
 import imageReducer from './image';
+import commentReducer from './comment';
 
 const rootReducer = combineReducers({
    session: sessionReducer,
-   image: imageReducer
+   image: imageReducer,
+   comment: commentReducer
 });
 
 let enhancer;
