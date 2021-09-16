686c11f99971c40e010cd8fede32a979a287aa1d
Alwyn Grant <marzgrant@gmail.com>
2021-09-16 16:20:02 -0700

    updated signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -9,18 +9,30 @@ const removeUser = () => { return { type: REMOVE_USER } };
 
 
 export const signup = (user) => async (dispatch) => {
-    const { username, email, password } = user;
-    const response = await csrfFetch("/api/user", {
+    const { images, image, username, email, password } = user
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("email", email);
+    formData.append("password", password);
+
+    // for multiple files
+    if (images && images.length !== 0) {
+        for (var i = 0; i < images.length; i++) {
+            formData.append("images", images[i]);
+        }
+    }
+
+    // for single file
+    if (image) formData.append("image", image);
+
+    const res = await csrfFetch(`/api/users/`, {
         method: "POST",
-        body: JSON.stringify({
-            username,
-            email,
-            password,
-        }),
+        headers: { "Content-Type": "multipart/form-data" },
+        body: formData,
     });
-    const data = await response.json();
+
+    const data = await res.json();
     dispatch(setUser(data.user));
-    return response;
 };
 
 
