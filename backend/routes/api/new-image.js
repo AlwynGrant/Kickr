6c5d5d9474505593f8da7e0446985815cfe734c7
Alwyn Grant <marzgrant@gmail.com>
2021-09-15 20:51:14 -0700

    added backend route for new comment

--- backend/routes/api/new-image.js
+++ backend/routes/api/new-image.js
@@ -2,13 +2,20 @@ const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { check, validationResult } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { Image } = require('../../db/models');
+const { Image, Comment } = require('../../db/models');
 const router = express.Router();
 
 const validateImage = [
     check('imageUrl')
         .exists({ checkFalsy: true })
         .withMessage('Please provide a valid url.'),
+    handleValidationErrors
+];
+
+const validateComment = [
+    check('comment')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a valid comment.'),
     handleValidationErrors
 ];
 
@@ -27,7 +34,7 @@ router.post("/", validateImage , asyncHandler(async (req, res) => {
             const errors = validationErrors.array().map((error) => error.msg);
             return res.json(errors)
         }
-}))
+}));
 
 // edit image content
 router.patch('/:id(\\d+)/edit', asyncHandler(async (req, res) => {
@@ -45,6 +52,25 @@ router.delete('/:id(\\d+)/delete', asyncHandler(async (req, res) => {
     // const { imageId } = image;
     await image.destroy();
     return res.json({ deleted: 'deleted!' })
+}));
+
+
+
+
+// new comment
+router.post("/:id(\\d+)/comment", validateComment, asyncHandler(async (req, res) => {
+
+    const { userId, imageId, comment } = req.body;
+    const newComment = await Comment.build({ userId, imageId, comment });
+    const validationErrors = validationResult(req);
+
+    if (validationErrors.isEmpty()) {
+        await newComment.save();
+        return res.json(newComment);
+    } else {
+        const errors = validationErrors.array().map((error) => error.msg);
+        return res.json(errors)
+    }
 }));
 
 
